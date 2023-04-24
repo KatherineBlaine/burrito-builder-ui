08acaa890e8dadbd3659c00f2e6764aee9af33e0
Katherine Blaine <blaine.katherine12@gmail.com>
2023-04-24 11:03:18 -0600

    test: update fixtures and add basic testing for main page DOM view on page load

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,34 @@
 describe('Main Display', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+  beforeEach(() => {
+    cy.intercept('GET', 'http://localhost:3001/api/v1/orders', {
+      statusCode: 200,
+      fixture: 'orders.json'
+    })
+    cy.visit('http://localhost:3000')
+
+  })
+
+  it('Should display the site title, order form, and orders elements on the page', () => {
+    cy.get('h1').should('be.visible').contains('Burrito Builder')
+    cy.get('.App').should('be.visible')
+    cy.get('form').should('be.visible')
+    cy.get('section').should('be.visible')
+  })
+
+  it('Should display all current orders on page load. Each should display the name and ingredients list for that order.', () => {
+    cy.get('section').children().should('have.length', 2)
+
+    cy.get('section > :nth-child(1)').should('be.visible')
+    cy.get(':nth-child(1) > h3').should('be.visible').contains('Heather')
+    cy.get(':nth-child(1) > .ingredient-list > :nth-child(1)').should('be.visible').contains('carnitas')
+    cy.get(':nth-child(1) > .ingredient-list > :nth-child(2)').should('be.visible').contains('pico de gallo')
+    cy.get(':nth-child(1) > .ingredient-list > :nth-child(3)').should('be.visible').contains('sour cream')
+
+    cy.get('section > :nth-child(2)').should('be.visible')
+    cy.get(':nth-child(2) > h3').should('be.visible').contains('Nik')
+    cy.get(':nth-child(2) > .ingredient-list > :nth-child(1)').should('be.visible').contains('beans')
+    cy.get(':nth-child(2) > .ingredient-list > :nth-child(2)').should('be.visible').contains('jalapenos')
+    cy.get(':nth-child(2) > .ingredient-list > :nth-child(3)').should('be.visible').contains('queso fresco')
+
   })
 })
