d81d22b03094a6734c29c8a5db070614a43ba1e2
Katherine Blaine <blaine.katherine12@gmail.com>
2023-04-24 09:44:16 -0600

    refac: refactor existing order form code to use hooks

--- src/components/OrderForm/OrderForm.js
+++ src/components/OrderForm/OrderForm.js
@@ -1,34 +1,26 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 
-class OrderForm extends Component {
-  constructor(props) {
-    super();
-    this.props = props;
-    this.state = {
-      name: '',
-      ingredients: []
-    };
-  }
+const OrderForm = () => {
+  const [name, setName] = useState('')
+  const [ingredients, setIngredients] = useState([])
 
 
   handleSubmit = e => {
     e.preventDefault();
-    this.clearInputs();
+    clearInputs();
   }
 
   clearInputs = () => {
-    this.setState({name: '', ingredients: []});
+    setName('');
+    setIngredients([]);
   }
-
-  render() {
     const possibleIngredients = ['beans', 'steak', 'carnitas', 'sofritas', 'lettuce', 'queso fresco', 'pico de gallo', 'hot sauce', 'guacamole', 'jalapenos', 'cilantro', 'sour cream'];
     const ingredientButtons = possibleIngredients.map(ingredient => {
       return (
-        <button key={ingredient} name={ingredient} onClick={e => this.handleIngredientChange(e)}>
+        <button key={ingredient} name={ingredient} onClick={e => handleIngredientChange(e)}>
           {ingredient}
         </button>
-      )
-    });
+      )});
 
     return (
       <form>
@@ -37,19 +29,18 @@ class OrderForm extends Component {
           placeholder='Name'
           name='name'
           value={this.state.name}
-          onChange={e => this.handleNameChange(e)}
+          onChange={e => handleNameChange(e)}
         />
 
         { ingredientButtons }
 
-        <p>Order: { this.state.ingredients.join(', ') || 'Nothing selected' }</p>
+        <p>Order: { ingredients.join(', ') || 'Nothing selected' }</p>
 
-        <button onClick={e => this.handleSubmit(e)}>
+        <button onClick={e => handleSubmit(e)}>
           Submit Order
         </button>
       </form>
     )
   }
-}
 
 export default OrderForm;
